7bc80f988adc7ddf9ffaf4d26e0103dfd797b33d
Washingto Pires <4washingtonbr@gmail.com>
2020-01-03 20:50:44 -0300

    Implementando o CRUD de Produto

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices]DataContext context)
+        {
+            var products = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById(
+            int id,
+            [FromServices]DataContext context
+        )
+        {
+            var product = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return product;
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory(
+            int id,
+            [FromServices]DataContext context
+        )
+        {
+            var products = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .Where(x => x.CategoryId == id)
+                .ToListAsync();
+
+            return products;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post(
+            [FromServices]DataContext context,
+            [FromBody]Product model
+        )
+        {
+            if(ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
